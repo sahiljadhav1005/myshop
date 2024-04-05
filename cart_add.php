568e234fbb1fd5a83ce3789dc5c44d29993332e3
SAHIL JADHAV <121169630+sahiljadhav1005@users.noreply.github.com>
2024-04-06 00:30:59 +0530

    Add files via upload

---
+++ cart_add.php
@@ -0,0 +1,13 @@
+<?php
+require 'connection.php';
+//require 'header.php';
+session_start();
+$item_id = $_GET['id'];
+$user_id = $_SESSION['id'];
+
+// Insert into users_items table
+$add_to_cart_query = "INSERT INTO users_items (user_id, item_id, status) VALUES ('$user_id', '$item_id', 'Added to cart')";
+$add_to_cart_result = mysqli_query($con, $add_to_cart_query) or die(mysqli_error($con));
+
+header('location: products.php');
+?>
---
+++ connection.php
@@ -0,0 +1,4 @@
+<?php
+// $con = mysqli_connect("ftpupload.net", "if0_36313906", "0TKhTLzb61iXGp", "if0_36313906_store") or die(mysqli_error($con));
+$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
+?>
